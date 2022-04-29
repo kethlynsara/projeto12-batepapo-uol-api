25d64816bd1d7f288c7bfda420d22f9b18ce9cc4
Kethlyn <kethlynsaraa@gmail.com>
2022-04-29 00:19:04 -0300

    POST /participants: impedir o cadastro de um  nome que já está sendo utilizado (retornar status 409)

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ import express from 'express';
 import cors from 'cors';
 import Joi from 'joi';
 import schema from './src/validation.js';
+import dotenv from 'dotenv';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+dotenv.config();
 
-app.post("/participants", (req, res) => {
+const mongoClient = new MongoClient(process.env.MONGO_URL);
+let database = null;
+
+app.post("/participants", async (req, res) => {
     const body  = req.body;
     const validateName = schema.validate(body);
   
@@ -17,6 +22,23 @@ app.post("/participants", (req, res) => {
         res.sendStatus(422);
         return;
     }
+
+    try {
+        await mongoClient.connect();
+        database = mongoClient.db("bate-papo-uol");        
+
+        const user = await database.collection("participants").findOne(body);
+
+        if (user) {
+            res.sendStatus(409);
+            return;        
+        }
+        mongoClient.close();
+    } catch(e) {
+        console.log(e);
+        mongoClient.close();
+    }
+    res.sendStatus(201);
 });
 
-app.listen(5000, () => console.log("it's on"));
+app.listen(5000);
