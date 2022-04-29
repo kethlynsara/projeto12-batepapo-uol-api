36f4716d521c0bb374b9bd976910085ee20d5646
Kethlyn <kethlynsaraa@gmail.com>
2022-04-29 00:34:08 -0300

    POST /participants: salvar o participante com o mongodb

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
-import { MongoClient } from 'mongodb';
-import express from 'express';
-import cors from 'cors';
-import Joi from 'joi';
-import schema from './src/validation.js';
-import dotenv from 'dotenv';
+import { MongoClient } from "mongodb";
+import express from "express";
+import cors from "cors";
+import Joi from "joi";
+import dotenv from "dotenv";
+import schema from "./src/validation.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-dotenv.config();
 
+dotenv.config();
 const mongoClient = new MongoClient(process.env.MONGO_URL);
 let database = null;
 
 app.post("/participants", async (req, res) => {
-    const body  = req.body;
-    const validateName = schema.validate(body);
-  
-    if (validateName.error) {
-        res.sendStatus(422);
+  const { name } = req.body;
+
+  const validateName = schema.validate({name});
+
+  if (validateName.error) {
+    res.sendStatus(422);
+    return;
+  }
+
+  try {
+    await mongoClient.connect();
+    database = mongoClient.db("bate-papo-uol");
+
+    const user = await database.collection("participants").findOne({name});
+
+    if (user) {
+        res.sendStatus(409);
         return;
+    } else {
+        await database.collection("participants").insertOne({name, lastStatus: Date.now()});
     }
-
-    try {
-        await mongoClient.connect();
-        database = mongoClient.db("bate-papo-uol");        
-
-        const user = await database.collection("participants").findOne(body);
-
-        if (user) {
-            res.sendStatus(409);
-            return;        
-        }
-        mongoClient.close();
-    } catch(e) {
-        console.log(e);
-        mongoClient.close();
-    }
-    res.sendStatus(201);
+    mongoClient.close();
+  } catch (e) {
+    console.log(e);
+    mongoClient.close();
+  }
 });
 
 app.listen(5000);
